ebec638b1a247ca5fb3386d96e1fed4ace7e2bd2
Ahmet Karaboğa <130494000+akaraboga@users.noreply.github.com>
2024-10-08 23:23:45 +0300

    Add files via upload

---
+++ WhileOrnek/WhileOrnek/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileOrnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+
+            int i=1;    
+            while (i != 11)
+            {
+                Console.WriteLine(i+"-Yazılım işini hallederim");
+                i++;
+               
+            }
+
+            */
+
+
+
+            /*
+                int i = 1;
+
+                while(i <=20)
+                {
+                    Console.WriteLine(i);
+                    i++;
+                }
+            */
+
+
+            /*
+                int i = 1; //Dögüde donecek olan değeri tanımlıyoruz
+ 
+                while(i <= 20)//hangi sayıya kadar döneceğini donguye yazdık
+                {
+                    if(i % 2 == 0)//eğer i değeri 2 ile tam bolunuyorsa i yi ekrana yazdırıyoruz
+                    {
+                        Console.WriteLine("Birden 20 ye kadar olan çift sayilar :"+i);
+                    }
+                    i++;
+                }
+
+                */
+
+
+
+            /*
+
+            float i = 50;
+            float toplam = 0;  
+
+            while (i <= 150)
+            {
+                toplam += i;
+
+                i++;
+            }
+            Console.WriteLine("50 ile 150 arasındaki sayıların toplamı :" + toplam);
+
+            */
+
+            int i = 1; 
+
+            int TekSayi = 0; // Döngünün içinde  i nin bolumunden kalana göre tek sayimi , çift sayimi olduğunda artacak olan değişkenleri alıyoruz
+            int ÇiftSayi =0;
+
+            while (i <= 120)
+            {
+
+                if(i % 2 == 0)//2 ile bolumunden kalan 0 ise çiftsayi değişkeni ile i yi topluyoruz 
+                {
+                    ÇiftSayi += i;
+                }
+                else//değilse teksayi topluyoruz
+                {
+                    TekSayi += i;
+                }
+
+                i++;
+            }
+
+            Console.WriteLine("Tek sayilarin toplamı :"+TekSayi);//toplama işlemlerini ekrana yazdıralım
+            Console.WriteLine("Çift sayilarin toplamı :"+ÇiftSayi);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WhileOrnek/WhileOrnek/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WhileOrnek")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WhileOrnek")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a376ef5c-143a-4c41-97da-acd20fe63ca3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
